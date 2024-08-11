aa12c00c7bf5cb53d57de58a67c2ace7d838ebd8
Adams Kiptalam <adamskiptalam0@gmail.com>
2024-08-11 20:06:55 +0300

    Create Convert HTML Entities.js

---
+++ INTERMEDIATE ALGORITHM SCRIPTING/Convert HTML Entities.js
@@ -0,0 +1,18 @@
+/*
+Convert HTML Entities
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+*/
+
+function convertHTML(str) {
+  const htmlEntities = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&apos;'
+  };
+  return str.replace(/[&<>"']/g, match => htmlEntities[match]);
+}
+
+console.log(convertHTML("Dolce & Gabbana"));
+console.log(convertHTML("Hamburgers < Pizza < Tacos"));
